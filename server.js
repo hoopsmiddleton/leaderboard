28bffd3ed4392cde25a6c54886f5ff08fb778ed9
Matt Middleton <Matt.Middleton@ford.com>
2022-02-03 15:42:45 -0500

    Removed old comments and test code.  Cleaned up file.

--- server.js
+++ server.js
@@ -6,34 +6,29 @@ const courseRouter = require('./routes/course');
 
 const port = process.env.PORT || 3998
 
-//app.use(logger2)
-//app.use(logger1)
-
 app.use(express.json())
+
 app.use(golferRouter)
 app.use(courseRouter)
+
 app.set("view engine", "ejs")
-
-//app.get('/leaderboard', (req, res) => {
-//	console.log('/leaderboard route')
-//	res.render('pairings')
-//})
-
-//app.get('/leaderboard/v1/pairings', (req, res) => {
-//	res.render('pairings')
-//})
 
 
 app.listen(port, () => {
     console.log(`Server us up on port: ${port}`)
 })
 
-function logger1(req, res, next) {
-	console.log(req)
-	next()
-}
+// Testing out bcrypt
+// const bcrypt = require('bcryptjs')
 
-function logger2(req, res, next) {
-	console.log('Logger2')
-	next()
-}
+// const myf = async() => {
+// 	const pw = 'abc!123'
+// 	const hashpw = await bcrypt.hash(pw, 8)
+
+// 	console.log(`PW: ${pw} and Hash: ${hashpw}`)
+
+// 	const isMatch = await bcrypt.compare('abc!123', hashpw)
+// 	console.log(`Is Match: ${isMatch}`)
+// }
+
+// myf()
