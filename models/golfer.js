1505b308982f12c3f1a6230e70d98d979e92fa12
Matt Middleton <Matt.Middleton@ford.com>
2022-02-03 17:29:42 -0500

    Added email validator.  Added password field. Added bcrypt and findByCreditials code. Added pre save middleware to hash password.

--- models/golfer.js
+++ models/golfer.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const golferSchema = new mongoose.Schema({
 
@@ -8,19 +9,22 @@ const golferSchema = new mongoose.Schema({
 		required: true,
 		trim: true
 	},
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+		trim: true,
+		lowercase: true,
+		validate(v) {
+			if (!validator.isEmail(v)) {
+				throw new Error('Not a valid email')
+			}
+		}
+	},
 	password: {
 		type: String,
 		required: true,
-		trim: true,
-		validate(v) { 
-			if (!validator.isStrongPassword(v, {
-				minLength: 6,
-				minLowercase: 1,
-				minUppercase: 1
-				})) {
-				throw new Error('Password invalid')
-			}
-		}
+		trim: true
 	},
 	team: {
 		type: String,
@@ -41,6 +45,36 @@ const golferSchema = new mongoose.Schema({
 	timestamps: true
 })
 
+// Find Golfer with
+golferSchema.statics.findByCredentials = async (email, password) => {
+	
+	const golfer = await Golfer.findOne({email: email})
+	console.log(golfer)
+	if (!golfer) {
+		throw new Error('Unable to find Golfer')
+	}
+	
+	const isMatch = await bcrypt.compare(password, golfer.password)
+	
+	if (!isMatch) {
+		console.log(`isMatch: ${isMatch}`)
+		throw new Error('Unable to find Golfer')
+	}
+
+	return golfer
+}
+
+// Mongoose middleware that will run pre (before) 'save'
+// This middleware will hash the password and store as one-way hash instead of plain text.
+golferSchema.pre('save', async function(next) {
+	const golfer = this
+
+	if (golfer.isModified('password')) {
+		golfer.password = await bcrypt.hash(golfer.password, 8)
+	}
+
+	next()
+})
 
 const Golfer = mongoose.model('Golfer', golferSchema)
 
